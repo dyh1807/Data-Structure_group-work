a83f6a3667c10d9251c9287d3b99051d00a0c80c
maou <951144709@qq.com>
2022-06-04 23:01:43 +0800

    feat: add head file of Project1

---
+++ Project1/TraverseBoard.h
@@ -0,0 +1,32 @@
+/**
+ * @file TraverseBoard.h
+ * @author Dai Yihao(951144709@qq.com)
+ * @brief the head file
+ * @version 0.1
+ * @date 2022-06-04
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#pragma once
+
+#define YSE 1
+#define NO 0
+
+/**
+ * @brief the Date structure to use
+ *
+ */
+
+/**
+ *  the function to used in the project
+ *
+ */
+/*TODO: Add the input dfs: used to traverse the board*/
+void dfs();
+
+/*TODO: input flow*/
+void input();
+
+/*TODO: output flow*/
+void output();
